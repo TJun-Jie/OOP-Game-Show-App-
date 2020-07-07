2d2d6b8bbd4d8e37ebbb535050981b3ae41ecc31
TJun-Jie <tjunjie2000@gmail.com>
2020-07-07 21:12:02 +0800

    Added code comments

--- js/Game.js
+++ js/Game.js
@@ -2,42 +2,7 @@
  * Project 4 - OOP Game App
  * Game.js */
 
-  /* 
-Create game class
-constructor properties:
-missed: number of times a player guessed wrong. initial value = 0
-phrases: an array of 5 phrases. Phrases should not have numbers or puncutation 
-active phrase: current phrase playing. initial value = null (will be set in the startgame method)
 
-startGame():
-hide start screen
-call getranodmphrase
-set active phrase 
-add active phrase to display using addphrasetodisplay()
-
-getRandomPhrase():
-randomly retrieve 1 phrase from phrases and returns it 
-
-handleInteraction(): 
-checks to see if the letter pressed by the player is correct anot
-after user clicks it, disable the letter button
-if it is wrong, add the wrong class to the selected letter button and then call the remove life method
-if it is correct, then call the showMatchedLetter method from the phrase class and then call checkForWin()
-if player has won, call the gameOver() method.
-
-removeLife()
-removes a life from the score board by replacing a liveHeart picture with a lostHeart (starts with 5 live heart images)
-add the missed property by 1 
-if the player has 5 missed properties, call the gameover method
-
-checkForWin():
-check to see if the player has guessed all the letters
-
-gameOver(): 
-if user win, display winning screen
-if lose, display losing screen
-
- */
 
  class Game {
      constructor() {
@@ -55,6 +20,7 @@ if lose, display losing screen
 
      }
 
+    //  get a random phrase and set the active phrase with it
      getRandomPhrase() {
         const randomNumber = Math.floor(Math.random() * this.phrases.length)
         this.activePhrase = this.phrases[randomNumber];
@@ -131,25 +97,27 @@ if lose, display losing screen
             }
 
         }
-        // reset buttons
+        // reset buttons to their initial state
         const correctButtons = document.querySelectorAll('.chosen');
         for (let i = 0; i < correctButtons.length; i ++) {
             correctButtons[i].classList.remove('chosen')
             correctButtons[i].disabled = false;
         }
-        
+
         const wrongButtons = document.querySelectorAll('.wrong');
         for (let i = 0; i < wrongButtons.length; i ++) {
             wrongButtons[i].classList.remove('wrong')
             wrongButtons[i].disabled = false;
         }
 
-        // reset heart
+        // reset heart to its initial state
         const hearts = document.querySelectorAll('.tries')
         for (let i = 0 ; i < hearts.length; i ++) {
             hearts[i].children[0].src = 'images/liveHeart.png'
         }
         this.missed = 0;
+
+        // create new phrase for user and display it 
         this.getRandomPhrase();
         this.activePhrase.addPhraseToDisplay();
 
--- js/Phrase.js
+++ js/Phrase.js
@@ -26,18 +26,22 @@ select all DOM elements with the class of match letters and replace the hide cla
          this.letters = this.removeSpaces();     
      }
 
+     
      addPhraseToDisplay() {
          const ul = document.querySelector('#phrase ul');
-         
+        //  create Li for each letter and set the text content of Li as the letter
          for (let i = 0 ; i < this.phrase.length ; i ++) {
             const li  = document.createElement('li');
+            // if the Li is a space add the space class
             if (this.phrase[i] === " ") {
                 li.classList.add('space');
             } 
+            // if the li is a letter add the letter class
             else {
                 li.classList.add('letter')
             }
             li.textContent = this.phrase[i]
+            // hide the letter from user
             li.classList.add('hide');
             ul.appendChild(li);      
          }
@@ -59,18 +63,19 @@ select all DOM elements with the class of match letters and replace the hide cla
                 if(this.letters[i] === letter) {
                     this.letters.splice(i, 1) ; i -- 
                 }
-
              }
              return true
+        // if letter not in the pharse return false
          } else {
              return false
          }
      }
 
      showMatchedLetter(letter) {
-        // select all DOM elements with the class of match letters and replace the hide class with the show class for each elements
+        // select all the letters on the display
         const displayLetters  =document.querySelectorAll('#phrase li');
         for (let i = 0 ; i < displayLetters.length ; i ++) {
+            // if the user guess correctly , change the class from hide to show
             if (displayLetters[i].textContent === letter) {
                 displayLetters[i].classList.remove('hide')
                 displayLetters[i].classList.add('show')
